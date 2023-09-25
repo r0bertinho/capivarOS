4994832a4ce3f57935d2edbafa6f2edce85ef621
Ian Souza Freire <112725039+r0bertinho@users.noreply.github.com>
2023-09-25 13:55:40 -0300

    Ian

--- Boot/boot.asm
+++ Boot/boot.asm
@@ -1,4 +1,4 @@
-jmp $
+jmp $                                                   ;?  jumping to $
 
-times 510-($-$$) db 0
-db 0x55, 0xaa
+times 510-($-$$) db 0                                   ;   defining byte 0 various times
+db 0x55, 0xaa                                           ;   adding the two final numbers to reach 512
