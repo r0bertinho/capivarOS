dc9fb392351e349003da23c58e8bfc4cd800b1fc
Ian Souza Freire <112725039+r0bertinho@users.noreply.github.com>
2023-09-26 17:55:47 -0300

    Ian

--- Boot/boot.asm
+++
@@ -1,4 +0,0 @@
-jmp $                                                   ;?  jumping to $
-
-times 510-($-$$) db 0                                   ;   defining byte 0 various times
-db 0x55, 0xaa                                           ;   adding the two final numbers to reach 512
---
+++ src/impl/kernel/main.c
@@ -0,0 +1,9 @@
+#include "D:\ZGAMES\zprogramacao\__VSCODE\projects\CapiVar_OS-master\src\impl\intf\print.h"
+
+
+void kernel_main()
+{
+    print_clear();
+    print_set_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
+    print_str("Welcome to the 64-bit kernel!");
+}
